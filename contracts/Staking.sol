9de5c36c8cf3ce987b8f9dd8a1d82a132dc6b0ec
harahash <alexanderkharakhash@gmail.com>
2022-01-31 12:07:10 +0200

    added natspec for staking contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,11 +4,14 @@ pragma solidity ^0.8.2;
 import "./TKNToken.sol";
 import "hardhat/console.sol";
 
+/// @title Staking contract
+/// @author Kharakhash A.
+/// @notice Contract caller can stake, unstake TKN tokens
 contract Staking {
     TKNToken public tknToken;
     uint256 public contractBalance;
-    uint256 public totalDepositedAmount; // T
-    uint256 public totalStakedAmount; // S
+    uint256 public totalDepositedAmount;
+    uint256 public totalStakedAmount;
     uint256 public depositedAmount;
     uint256 public stakedReward;
     uint256 public withdrawAmount;
@@ -19,6 +22,9 @@ contract Staking {
         tknToken = _tknToken;
     }
 
+    /// @notice Take entered amount of tokens to stake
+    /// @dev Saves the amount of tokens and 'time' they were pushed in
+    /// @param _amount Amount of inserted tokens for staking
     function deposit(uint256 _amount) public payable {
         require(_amount > 0, "amount cannot be 0");
         tknToken.transferFrom(msg.sender, address(this), _amount);
@@ -27,6 +33,8 @@ contract Staking {
         totalDepositedAmount = totalDepositedAmount + _amount;
     }
 
+    /// @notice Distribute reward to the stakers
+    /// @param _reward Amount of tokens that will be distributed proportionally between all stakers
     function distribute(uint256 _reward) public {
         require(totalDepositedAmount != 0, "there is no stakers at this point");
         totalStakedAmount =
@@ -34,6 +42,8 @@ contract Staking {
             (_reward / totalDepositedAmount);
     }
 
+    /// @notice Process of unstaking all tokens
+    /// @dev Cheking if the address has staked tokens
     function withdrawAll() public {
         depositedAmount = stakeBalance[msg.sender];
         require(depositedAmount > 0, "you have no tokens to withdraw");
@@ -41,6 +51,8 @@ contract Staking {
         rewardAndSend(depositedAmount, msg.sender);
     }
 
+    /// @notice Process of unstaking certain amount of tokens
+    /// @dev Checking if the address has staked tokens and whether wants to unstake more than available
     function partialWithdrawal(uint256 _amount) public {
         depositedAmount = stakeBalance[msg.sender];
         require(depositedAmount > 0, "you have no tokens to withdraw");
@@ -52,6 +64,9 @@ contract Staking {
         rewardAndSend(_amount, msg.sender);
     }
 
+    /// @notice Calculating reward and send it to account
+    /// @param _amount Amount of tokens that was unstaked
+    /// @param _recepient The account to send tokens to
     function rewardAndSend(uint256 _amount, address _recepient) private {
         stakedReward =
             _amount *
@@ -62,6 +77,9 @@ contract Staking {
         updateBalance(_amount, _recepient);
     }
 
+    /// @notice Calculating reward and send it to account
+    /// @param _amount Amount of tokens that need to subtracted from total amount
+    /// @param _owner The account from which the tokens will be taken away
     function updateBalance(uint256 _amount, address _owner) private {
         totalDepositedAmount = totalDepositedAmount - _amount;
         stakeBalance[_owner] = stakeBalance[_owner] - _amount;
